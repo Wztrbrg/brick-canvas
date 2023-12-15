c96c46ee4ef553b7f1cd59df56244c01b2e3cbc1
Jonathan <2jonand@gmail.com>
2023-12-15 16:01:17 +0100

    setup server and db

---
+++ backend/config/db.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on("error", (err) => {
+  console.error(`MongoDB connection error: ${err}`);
+});
+
+db.once("open", () => {
+  console.log("Connected to MongoDB");
+});
+
+export default db;
