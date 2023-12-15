c8111c437fc3a230614d12c3627f212e8b310942
Jonathan <2jonand@gmail.com>
2023-12-15 16:34:08 +0100

    basic backend structure for uploading images

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,25 @@
 import express from "express";
+import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors";
+import itemRoutes from "./routes/itemRoutes.js";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3001;
+const mongodb = process.env.MONGODB_URI;
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use("/items", itemRoutes);
+app.get("/", (req, res) => {
+  res.send("Hello");
 });
+
+mongoose
+  .connect(mongodb)
+  .then(() => app.listen(port, console.log(`Server running on ${port}`)))
+  .catch((err) => console.log(err));
