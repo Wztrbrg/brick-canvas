9838e5f43f2a47726c70585003030b110084a92a
Jonathan <2jonand@gmail.com>
2024-01-10 16:24:25 +0100

    finalized thank-you-page

--- src/pages/ThanksPage.jsx
+++ src/pages/ThanksPage.jsx
@@ -1,6 +1,19 @@
+import Header from "../components/Header"
+import "./thankspage.css";
+
 function ThanksPage() {
   return (
-    <h1>Tack!</h1>
+    <>
+      <Header></Header>
+      <div className="thanks-page-wrapper">
+        <div className="thanks-content">
+          <div className="thanks-header">
+            <h1 className="thanks-heading">Tack för din beställning 🎉</h1>
+            <h3 className="thanks-subheading">En bekräftelse på din order skickas omgående till din e-post. Vi hoppas att du blir nöjd med ditt Lego-mästerverk, och du är välkommen åter!</h3>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
